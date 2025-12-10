a17c824a38f0091d9b6395a750c5763813c9604c
Jared Coen <jared.coen1@gmail.com>
2025-12-09 17:46:33 -0700

    v3 Added date time to each comment

--- src/main/java/edu/shadsluiter/comments/model/AppComment.java
+++ src/main/java/edu/shadsluiter/comments/model/AppComment.java
@@ -1,53 +1,67 @@
 package edu.shadsluiter.comments.model;
 
 import java.time.LocalDateTime;
- 
 
-public class AppComment {
-    private int id;
-    private String author;
-    private String content; 
+public class AppComment
+{
+	private int id;
+	private String author;
+	private String content;
+	private LocalDateTime createdAt;
 
-    public AppComment(int id, String author, String content ) {
-        this.id = id;
-        this.author = author;
-        this.content = content; 
-    }
+	public AppComment(int id, String author, String content, LocalDateTime createdAt)
+	{
+		this.id = id;
+		this.author = author;
+		this.content = content;
+		this.createdAt = createdAt;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId()
+	{
+		return id;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public String getAuthor()
+	{
+		return author;
+	}
 
-    public String getContent() {
-        return content;
-    }
+	public String getContent()
+	{
+		return content;
+	}
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+	public void setContent(String content)
+	{
+		this.content = content;
+	}
 
-    public void setAuthor(String author) {
-        this.author = author;
-    }
+	public void setAuthor(String author)
+	{
+		this.author = author;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
- 
-    
+	public LocalDateTime getCreatedAt()
+	{
+		return createdAt;
+	}
 
-    @Override
+	public void setCreatedAt(LocalDateTime createdAt)
+	{
+		this.createdAt = createdAt;
+	}
 
-    public String toString() {
-        return "Comment{" +
-                "id=" + id +
-                ", author='" + author + '\'' +
-                ", content='" + content + '\'' + 
-                '}';
-    }
-    
+	public void setId(int id)
+	{
+		this.id = id;
+	}
+
+	@Override
+
+	public String toString()
+	{
+		return "AppComment [id=" + id + ", author=" + author + ", content=" + content + ", createdAt=" + createdAt + "]";
+	}
+
 }
